823a4ee7f0a5c6435d1227df2981b1bb2efb1e5a
arthu9 <narthurannthony@yahoo.com>
2018-03-18 04:55:42 +0800

    partial

--- BookshelfV2-mobile/www/js/my-app.js
+++ BookshelfV2-mobile/www/js/my-app.js
@@ -8,9 +8,7 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
-    domCache: true,
-    dynamicNavbar: true,
-
+    dynamicNavbar: true
 });
 
 // Handle Cordova Device Ready Event
@@ -27,21 +25,19 @@ myApp.onPageInit('about', function (page) {
 
 })
 
-
-
 // Option 2. Using one 'pageInit' event handler for all pages:
-// $$(document).on('pageInit', function (e) {
-//     // Get page data from event data
-//     var page = e.detail.page;
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
 
-//     if (page.name === 'about') {
-//         // Following code will be executed for page with data-page attribute equal to "about"
-//         myApp.alert('Here comes About page');
-//     }
-// })
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
 
-// // Option 2. Using live 'pageInit' event handlers for each page
-// $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-//     // Following code will be executed for page with data-page attribute equal to "about"
-//     myApp.alert('Here comes About page');
-// })
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
