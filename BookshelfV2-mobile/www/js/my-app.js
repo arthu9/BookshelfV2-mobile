08a5fd7f80e6d75c9931197dc1fcf6b446188ae6
arthu9 <narthurannthony@yahoo.com>
2018-03-21 14:29:42 +0800

    signup partial

--- BookshelfV2-mobile/www/js/my-app.js
+++ BookshelfV2-mobile/www/js/my-app.js
@@ -8,7 +8,8 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+    dynamicNavbar: true,
+    domCache: true,
 });
 
 // Handle Cordova Device Ready Event
