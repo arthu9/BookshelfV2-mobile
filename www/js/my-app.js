b5cd10394242deac71b180456dcf118b10c259c2
jrldtlld <31199549+jrldtlld@users.noreply.github.com>
2018-04-04 10:37:43 +0800

    Improved the dashboard
    added carousel and load a sample image setup clickable links and sequences.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,19 +15,3 @@ var mainView = myApp.addView('.view-main', {
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
-
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-})
-
